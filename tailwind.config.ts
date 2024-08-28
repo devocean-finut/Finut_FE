09179777a3ba09228d9b0f321bc56682c143f0ac
Sung Chul Park <jasonsc@naver.com>
2024-08-28 15:40:05 +0900

    Update tailwind.config : Add service custom color pallette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,32 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      character: "#FFC89D",
+      primary: "#8C6C56",
+      background: "#FFF5EB",
+      "modal-background": "#D9D9D966",
+      gray: {
+        10: "#F6F6F6",
+        20: "#E8E8E8",
+        30: "#BDBDBD",
+        40: "#666666",
+      },
+      red: {
+        primary: "#FF1F1F",
+        secondary: "#FF4545",
+      },
+      blue: {
+        primary: "#1976D2",
+        secondary: "#1E88E5",
+      },
+      green: {
+        primary: "#43A047",
+        secondary: "#C5FFC7",
+      },
+    },
   },
   plugins: [],
 };
