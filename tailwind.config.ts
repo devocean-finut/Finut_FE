e4814fd0b53d2e711f7bcf866052ce6010f1678c
Sung Chul Park <jasonsc@naver.com>
2024-10-29 04:38:03 +0900

    Update tailwind.config : 컴포넌트 파일 경로 추가, colors 위치 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,31 +14,31 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      white: "#FFFFFF",
-      black: "#000000",
-      character: "#FFC89D",
-      primary: "#8C6C56",
-      background: "#FFF5EB",
-      "modal-background": "#D9D9D966",
-      gray: {
-        10: "#F6F6F6",
-        20: "#E8E8E8",
-        30: "#BDBDBD",
-        40: "#666666",
-      },
-      red: {
-        primary: "#FF1F1F",
-        secondary: "#FF4545",
-      },
-      blue: {
-        primary: "#1976D2",
-        secondary: "#1E88E5",
-      },
-      green: {
-        primary: "#43A047",
-        secondary: "#C5FFC7",
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        character: "#FFC89D",
+        primary: "#8C6C56",
+        background: "#FFF5EB",
+        "modal-background": "#D9D9D966",
+        gray: {
+          10: "#F6F6F6",
+          20: "#E8E8E8",
+          30: "#BDBDBD",
+          40: "#666666",
+        },
+        red: {
+          primary: "#FF1F1F",
+          secondary: "#FF4545",
+        },
+        blue: {
+          primary: "#1976D2",
+          secondary: "#1E88E5",
+        },
+        green: {
+          primary: "#43A047",
+          secondary: "#C5FFC7",
+        },
       },
     },
   },
