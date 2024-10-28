51d3664d0c704b596b4cc0799da996e1fa185ba6
Sung Chul Park <jasonsc@naver.com>
2024-10-29 04:57:35 +0900

    Update Social Login Buttons : Svg 속성 camelCase로 변경

--- src/components/Button/NaverButton.tsx
+++ src/components/Button/NaverButton.tsx
@@ -11,7 +11,7 @@ function NaverButton() {
         xmlns="http://www.w3.org/2000/svg"
       >
         <rect width="60" height="60" rx="30" fill="#03C75A" />
-        <g clip-path="url(#clip0_17_383)">
+        <g clipPath="url(#clip0_17_383)">
           <path
             d="M33.5614 30.7033L26.1461 20H20V40H26.4386V29.295L33.8539 40H40V20H33.5614V30.7033Z"
             fill="white"
