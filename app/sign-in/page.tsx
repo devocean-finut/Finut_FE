0e2fd205edcbd92470c3ca277dbed96fa3d98488
Sung Chul Park <jasonsc@naver.com>
2024-10-31 13:53:11 +0900

    Update sign-in : 로그인, redirection 테스트

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,13 +5,44 @@ import GoogleButton from "@/src/components/Button/GoogleButton";
 import KakaoButton from "@/src/components/Button/KakaoButton";
 import NaverButton from "@/src/components/Button/NaverButton";
 import { Input } from "@/src/components/Input";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 function Page() {
+  const router = useRouter();
   const [loginInfo, setLoginInfo] = useState({
     id: "",
     password: "",
   });
+
+  const handleLogin = () => {
+    if (!loginInfo.id || !loginInfo.password) {
+      alert("아이디와 비밀번호를 입력해주세요.");
+      return;
+    }
+    if (loginInfo.id === "test" && loginInfo.password === "test") {
+      try {
+        fetch("/api/test-login", {
+          method: "POST",
+          credentials: "include", // 쿠키를 포함하여 요청, 응답에서 수신할 수 있게 함
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.ok) {
+            router.push("/main");
+          } else {
+            alert("로그인 실패!");
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+      return;
+    }
+    alert("존재하지 않는 아이디입니다.");
+    return;
+  };
   return (
     <div className="w-full h-screen flex flex-col gap-8 items-center justify-center">
       <div className="mt-auto">
@@ -42,7 +73,7 @@ function Page() {
         backgroundColor="primary"
         color="white"
         weight="bold"
-        onClick={() => console.log("clicked")}
+        onClick={handleLogin}
       >
         이메일로 로그인
       </Button>
