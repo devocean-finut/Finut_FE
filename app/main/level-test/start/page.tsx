0b501c1d4cf9aaad6b44feaed2991c8953dfbf99
Sung Chul Park <jasonsc@naver.com>
2024-10-31 14:53:43 +0900

    Add level-test/start : 레벨테스트 시작 페이지

---
+++ app/main/level-test/start/page.tsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from "@/src/components/Levet-test/WelcomeMessage";
+import React, { useEffect, useState } from "react";
+
+function Page() {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default Page;
