631f9b9144085d540ec96c76e1a2e8abeefee1be
Sung Chul Park <jasonsc@naver.com>
2024-10-31 09:52:32 +0900

    Add middleware : 로그인 여부 홈 라우트 추가

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedRoutes = ["/main"]; // 로그인 정보가 있어야만 접근할 수 있는 페이지
+const protectedRoutes = ["/main", "/"]; // 로그인 정보가 있어야만 접근할 수 있는 페이지
 const publicRoutes = ["/sign-in", "/sign-up"];
 
 export function middleware(request: NextRequest) {
