c32d70d8e5517fc14e5b39a548f2fa69e39c72f3
Sung Chul Park <jasonsc@naver.com>
2024-11-07 22:59:04 +0900

    Update main page : 로직 확인 중일 때 로딩 컴포넌트 보여지게 수정

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,23 +1,27 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import LoadingSpinner from "@/src/components/LoadingSpinner";
 
 function Page() {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const LOGIN_INFO = localStorage.getItem("LOGIN_INFO");
     try {
       const userObject = LOGIN_INFO ? JSON.parse(LOGIN_INFO) : null;
       if (userObject.isFirst) {
         router.push("/main/level-test/start");
+      } else {
+        setIsLoading(false);
       }
     } catch (error) {
       alert("로그인 후 이용해주세요.");
       router.push("/sign-in");
     }
   }, []);
-
+  if (isLoading) return <LoadingSpinner />;
   return <div>Page</div>;
 }
 
