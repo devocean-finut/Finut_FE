a2a1d500a594c1ea978fa47b86334f07406fed0a
Sung Chul Park <jasonsc@naver.com>
2024-11-11 15:37:08 +0900

    Update MainFooter : 아이디, 레벨, 보유 코인, 경험치 등 UI 수정

--- src/Main/MainFooter.tsx
+++ src/Main/MainFooter.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CoinIcon from "./Icon/Coin";
 import ExpIcon from "./Icon/Exp";
+import ExpBar from "./components/ExpBar";
 
 function MainFooter() {
+  useEffect(() => {
+    // 현재 계급, 경험치, 보유 코인 정보 서버에서 가져오기
+  }, []);
   return (
     <footer className="flex flex-col gap-4 bg-white p-4 rounded-xl drop-shadow-sm">
-      <span>현재 계급 : 인턴</span>
+      <div className="w-full flex justify-between items-center">
+        {/* 아이디 */}
+        <span className="font-semibold text-2xl">
+          여기까지가끝인가보오<span className="text-lg font-normal">님</span>
+        </span>
+        <span className="text-primary font-bold">인턴</span>
+      </div>
       <div className="flex gap-2 items-center">
         <CoinIcon /> 5,000
       </div>
-      <div className="flex gap-2 items-center">
-        <ExpIcon /> 90/100
+      <div className="w-full flex gap-2 items-start">
+        <ExpIcon /> <ExpBar currentExp={50} maxExp={100} />
       </div>
     </footer>
   );
