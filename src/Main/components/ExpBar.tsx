8af251e0d0b6589bb0a97ad84367e906e74ff157
Sung Chul Park <jasonsc@naver.com>
2024-11-11 15:36:35 +0900

    Add ExpBar : 현재 경험치 표시 바로 표시

---
+++ src/Main/components/ExpBar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+type ExpBarProps = {
+  currentExp: number;
+  maxExp: number;
+};
+
+function ExpBar({ currentExp = 0, maxExp = 100 }: ExpBarProps) {
+  const [experience, setExperience] = useState<number>(0); // 경험치 상태와 타입 정의
+
+  const experiencePercent = ((currentExp / maxExp) * 100).toFixed(2);
+
+  return (
+    <div className="w-full h-full flex flex-col ">
+      <div className="w-full flex gap-2">
+        <div className="w-full h-6 rounded-full border-2 border-primary overflow-hidden">
+          <div
+            className="h-full bg-primary flex items-center justify-end pr-2 font-bold"
+            style={{
+              background:
+                "linear-gradient(to right, #A07E68, #8C6C56, #5F493E)",
+              width: `${experiencePercent}%`,
+            }}
+          ></div>
+        </div>
+        <div className="font-medium">{experiencePercent}%</div>
+      </div>
+      <div className="w-full flex justify-end gap-1 text-sm pt-2 ">
+        승진까지 <span className="font-bold">{maxExp - currentExp} XP</span>
+      </div>
+    </div>
+  );
+}
+
+export default ExpBar;
