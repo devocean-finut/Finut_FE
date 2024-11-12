3faf202c1b1bcc0a1f215932c66e0e79f4390b0a
Sung Chul Park <jasonsc@naver.com>
2024-11-12 10:12:58 +0900

    Update MainCharacter : 유저 이름 표시 및 마이페이지 라우팅 연결

--- src/Main/MainCharacter.tsx
+++ src/Main/MainCharacter.tsx
@@ -3,14 +3,31 @@ import { Button } from "../Common/Button";
 import ShopIcon from "./Image/Shop";
 import Image from "next/image";
 import Character from "./Image/Character.png";
+import PencilIcon from "./Image/Pencil";
+import { useRouter } from "next/navigation";
 
 function MainCharacter() {
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push("/main/mypage");
+  };
   return (
-    <main className="flex flex-col items-center gap-2">
-      <div className="w-[200px] aspect-square relative">
+    <main className="flex flex-col items-center gap-1">
+      <div className="max-w-[250px] w-full min-w-[200px] aspect-square relative mb-2">
         <Image src={Character} alt="캐릭터.png" layout="fill" />
       </div>
-      <div>세상에서 제일 가는 피넛</div>
+      <div className="flex items-center font-semibold text-xl">
+        여기까지가끝인가보오{" "}
+        <Button
+          width="fit-content"
+          backgroundColor="background"
+          paddingHorizontal={6}
+          paddingVertical={4}
+          onClick={handleOnClick}
+        >
+          <PencilIcon />
+        </Button>
+      </div>
       <Button
         paddingVertical={6}
         paddingHorizontal={12}
