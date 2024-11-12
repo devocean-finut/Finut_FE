7ae4d53073ba3b1e750aeab796ab07aabb853f31
Sung Chul Park <jasonsc@naver.com>
2024-11-12 09:55:59 +0900

    Update MainCharacter : 캐릭터 이미지 파일 추가

--- src/Main/MainCharacter.tsx
+++ src/Main/MainCharacter.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Button } from "../Common/Button";
 import ShopIcon from "./Image/Shop";
+import Image from "next/image";
+import Character from "./Image/Character.png";
 
 function MainCharacter() {
   return (
     <main className="flex flex-col items-center gap-2">
-      <div>character</div>
+      <div className="w-[200px] aspect-square relative">
+        <Image src={Character} alt="캐릭터.png" layout="fill" />
+      </div>
       <div>세상에서 제일 가는 피넛</div>
       <Button
         paddingVertical={6}
