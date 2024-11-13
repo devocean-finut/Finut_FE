37ccbc1e4b54996864b1a3848ebd3a560898626a
Sung Chul Park <jasonsc@naver.com>
2024-11-13 22:43:35 +0900

    Update mypage : 유저 정보, 버튼 그룹 컴포넌트 추가

--- app/main/mypage/page.tsx
+++ app/main/mypage/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import MyPageButtonGroup from "@/src/My-Page/ButtonGroup";
+import MyPageUserInfo from "@/src/My-Page/UserInfo";
 
 function Page() {
-  return <div>Page</div>;
+  const userData = {
+    userId: 2,
+    name: "황지수 (plumking)",
+    money: 100000,
+    picture:
+      "https://lh3.googleusercontent.com/a/ACg8ocKxUDNyJdhvRA40sHE7EfbKYpi0yNMzWpWZKeFsxC6xCAenE9tuBA=s96-c",
+    xp: 20,
+    levelName: "아르바이트",
+  };
+  return (
+    <>
+      <h1 className="font-medium text-xl py-4">마이페이지</h1>
+      <div className="w-full flex flex-col items-center gap-6">
+        <MyPageUserInfo userData={userData} />
+        <MyPageButtonGroup />
+      </div>
+    </>
+  );
 }
 
 export default Page;
