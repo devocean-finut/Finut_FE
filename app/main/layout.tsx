fd44ca8cec299aaf3f9748cac745b47c9c844d0a
Sung Chul Park <jasonsc@naver.com>
2024-11-14 00:14:04 +0900

    Update main layout : 공통 레이아웃 padding 수정

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-background max-w-[600px] w-screen h-screen overflow-y-scroll px-6">
+    <div className="bg-background max-w-[600px] w-screen h-screen overflow-y-scroll p-6">
       {children}
     </div>
   );
