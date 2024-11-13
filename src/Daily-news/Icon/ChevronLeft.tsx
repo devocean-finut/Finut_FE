348ac691c429f49d3db818e4b48fb54be08645e8
Sung Chul Park <jasonsc@naver.com>
2024-11-13 13:39:27 +0900

    Add Chevron Left icon : 뒤로가기 화살표 아이콘 추가

---
+++ src/Daily-news/Icon/ChevronLeft.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ChevronLeft() {
+  return (
+    <div>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M15.41 16.58L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.58Z"
+          fill="black"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default ChevronLeft;
