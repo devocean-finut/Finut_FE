be871e69d029c31d3040d4bbbd2754f9f3ab29c6
Sung Chul Park <jasonsc@naver.com>
2024-11-14 23:21:45 +0900

    Update main page : 로그인 정보 처리 및 리다이렉트 로직 추가

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import LoadingSpinner from "@/src/Common/LoadingSpinner";
 import MainHeader from "@/src/Main/MainHeader";
 import MainCharacter from "@/src/Main/MainCharacter";
@@ -9,13 +9,20 @@ import MainFooter from "@/src/Main/MainFooter";
 
 function Page() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const email = searchParams.get("email");
+  const accessToken = searchParams.get("accessToken");
+  if (email && accessToken) {
+    localStorage.setItem("LOGIN_INFO", JSON.stringify({ email, accessToken }));
+    router.push("/main");
+  }
   const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const LOGIN_INFO = localStorage.getItem("LOGIN_INFO");
     try {
       const userObject = LOGIN_INFO ? JSON.parse(LOGIN_INFO) : null;
-      if (userObject.isFirst) {
-        router.push("/main/level-test/start");
+      if (!userObject) {
+        throw new Error("로그인 후 이용해주세요.");
       } else {
         setIsLoading(false);
       }
