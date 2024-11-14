a486c6723f61eb5aa408cbc6bd23e76c135c839a
Sung Chul Park <jasonsc@naver.com>
2024-11-14 22:46:15 +0900

    Update attendance page: 출석 체크 애니메이션을 Congratulation에서 Check로 변경

--- app/main/attendance/page.tsx
+++ app/main/attendance/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Lottie from "react-lottie-player";
-import CongratulationLottie from "@/public/lottie/Congratulation.json";
+import CheckLottie from "@/public/lottie/Check.json";
 import { useEffect, useState } from "react";
 import { Button } from "@/src/Common/Button";
 import { useRouter } from "next/navigation";
@@ -21,21 +21,21 @@ function Page() {
   return (
     <div className="w-full h-full flex items-center justify-center">
       {attendanceStep === 0 && (
-        <>
+        <div className="flex flex-col gap-6 justify-center items-center">
           <Lottie
-            className="absolute top-2/5 w-full"
+            className="w-1/2"
             loop={false}
-            animationData={CongratulationLottie}
+            animationData={CheckLottie}
             play={true}
           />
           <span className="fadeIn text-3xl">출석 체크 완료!</span>
-        </>
+        </div>
       )}
       {attendanceStep === 1 && (
-        <div className="flex flex-col gap-4">
-          <span className="fadeIn text-3xl">
-            5회 출석하면 월급을 받을 수 있어요!
-          </span>
+        <div className="w-full flex flex-col gap-6">
+          <div className="fadeIn text-3xl text-center">
+            5회 출석하면 <br /> 월급을 받을 수 있어요!
+          </div>
           <div className="fadeIn flex gap-4 bg-white p-4 rounded-xl shadow-sm">
             <span className="w-1/5 aspect-square  bg-blue-primary text-white rounded-full text-4xl flex justify-center items-center shadow-sm">
               ✓
