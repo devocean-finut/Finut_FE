76f9f22812b50d0434b087338a87f925684b7e5a
Sung Chul Park <jasonsc@naver.com>
2024-11-15 17:11:37 +0900

    Add LevelTestLayout

---
+++ app/main/level-test/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LevelTestLayout({ children }: { children: React.ReactNode }) {
+  return <div className="w-full h-full">{children}</div>;
+}
+
+export default LevelTestLayout;
