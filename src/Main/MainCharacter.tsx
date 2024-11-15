d7b11004c0aca4847d0b263253684c1d6ed9d5c7
Sung Chul Park <jasonsc@naver.com>
2024-11-15 10:12:16 +0900

    Update MainCharacter : 사용자 프로필 정보를 API로 가져오는 로직 추가 및 상태 관리 구현

--- src/Main/MainCharacter.tsx
+++ src/Main/MainCharacter.tsx
@@ -1,23 +1,66 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../Common/Button";
 import ShopIcon from "./Image/Shop";
 import Image from "next/image";
 import Character from "./Image/Character.png";
 import PencilIcon from "./Image/Pencil";
 import { useRouter } from "next/navigation";
+import { decode } from "js-base64";
+import { API_BASE_URL } from "../Constant/constant";
+
+type UserData = {
+  userId: number;
+  name: string;
+  money: number;
+  picture: string;
+  xp: number;
+  levelName: string;
+};
 
 function MainCharacter() {
   const router = useRouter();
+  const [userData, setUserData] = useState<UserData>({
+    userId: 0,
+    name: "string",
+    money: 0,
+    picture: "string",
+    xp: 0,
+    levelName: "string",
+  });
   const handleOnClick = () => {
     router.push("/main/mypage");
   };
+  useEffect(() => {
+    const LOGIN_INFO =
+      localStorage.getItem("LOGIN_INFO") || router.push("/sign-in");
+    if (LOGIN_INFO) {
+      const accessToken = decode(JSON.parse(LOGIN_INFO).accessToken);
+
+      fetch(`${API_BASE_URL}/user/profile`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.code === "COMMON200") {
+            setUserData(data.result);
+          } else if (data.code === "COMMON500") {
+            alert("로그인 후 이용해주세요.");
+            router.push("/sign-in");
+          }
+        });
+    }
+  }, []);
   return (
     <main className="flex flex-col items-center gap-1">
       <div className="max-w-[250px] w-full min-w-[200px] aspect-square relative mb-2">
         <Image src={Character} alt="캐릭터.png" layout="fill" />
       </div>
       <div className="flex items-center font-semibold text-xl">
-        여기까지가끝인가보오{" "}
+        {userData && userData.name}
         <Button
           width="fit-content"
           backgroundColor="background"
