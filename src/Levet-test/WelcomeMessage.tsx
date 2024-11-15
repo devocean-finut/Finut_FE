f1d73bc186495341c1bd48f6795919b6fed02c40
Sung Chul Park <jasonsc@naver.com>
2024-11-15 17:12:39 +0900

    Update WelcomeMessage: 환영 메시지 수정 및 버튼 스타일 개선

--- src/Levet-test/WelcomeMessage.tsx
+++ src/Levet-test/WelcomeMessage.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./WelcomeMessage.module.css";
 import { Button } from "../Common/Button";
 import { useRouter } from "next/navigation";
+import ChevronRight from "../My-Page/ChevronRight";
 
 function WelcomeMessage() {
   const router = useRouter();
@@ -11,7 +12,7 @@ function WelcomeMessage() {
   const [buttonShow, setButtonShow] = useState(false);
   const messages = [
     // "첫 방문을 진심으로 환영합니다!",
-    `레벨 테스트를 통해 \n여러분의 경제 실력을 확인해보세요.`,
+    `레벨 테스트를 통해 \n승진할 준비 되셨나요?`,
   ];
 
   useEffect(() => {
@@ -29,7 +30,7 @@ function WelcomeMessage() {
   }, [currentMessageIndex, messages.length]);
 
   return (
-    <div className="h-full flex flex-col items-center gap-6 pt-[60%] text-2xl">
+    <div className="h-full flex flex-col items-center justify-center gap-6 text-2xl">
       {messages.slice(0, currentMessageIndex).map((message, index) => (
         <p
           key={index}
@@ -40,15 +41,19 @@ function WelcomeMessage() {
       ))}
       <Button
         width="auto"
-        paddingHorizontal={32}
-        paddingVertical={16}
+        paddingHorizontal={24}
+        paddingVertical={8}
         backgroundColor="primary"
         color="white"
         fontSize="2xl"
-        className={`${buttonShow ? "block" : "hidden"} ${styles.fadeIn}`}
+        className={`${buttonShow ? "block" : "hidden"} ${
+          styles.fadeIn
+        } flex items-center justify-end`}
         onClick={() => router.push("/main/level-test")}
       >
-        테스트 {">"}
+        <div className="w-2"></div>
+        테스트 시작
+        <ChevronRight size={40} fill="white" />
       </Button>
     </div>
   );
