69113611cfad4dfec994ec9b34e7abbeae0b26ec
Sung Chul Park <jasonsc@naver.com>
2024-11-15 10:11:25 +0900

    Add constant : API base URL, level names

---
+++ src/Constant/constant.ts
@@ -0,0 +1,12 @@
+export const API_BASE_URL =
+  "http://ec2-15-165-175-91.ap-northeast-2.compute.amazonaws.com:8080";
+
+export const levelNames = [
+  "아르바이트",
+  "인턴",
+  "사원",
+  "대리",
+  "과장",
+  "차장",
+  "부장",
+];
