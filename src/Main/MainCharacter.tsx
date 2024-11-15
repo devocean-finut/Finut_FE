6c3a9d8a4370247a04058cb16054d9be9888d401
Sung Chul Park <jasonsc@naver.com>
2024-11-15 10:41:30 +0900

    Update MainCharacter: UserData 타입을 export로 변경

--- src/Main/MainCharacter.tsx
+++ src/Main/MainCharacter.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import { decode } from "js-base64";
 import { API_BASE_URL } from "../Constant/constant";
 
-type UserData = {
+export type UserData = {
   userId: number;
   name: string;
   money: number;
